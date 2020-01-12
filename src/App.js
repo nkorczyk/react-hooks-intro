ade71a116a7566eb6e316dfaa8b48bc1c85b387a
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-01-12 20:43:11 +0100

    Intro to hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import SongList from './components/SongList';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <SongList />
     </div>
   );
 }
---
+++ src/components/SongList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SongList = () => {
+  return <div className="song-list">
+    <ul>
+      <li>this wild darkness</li>
+      <li>memory gospell</li>
+    </ul>
+  </div>;
+};
+
+export default SongList;
